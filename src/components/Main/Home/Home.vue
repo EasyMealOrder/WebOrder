a07ff090fe6dbae4aa4f8b80867614c0acc7dd92
caijjh <caijh23@gmail.com>
2018-04-23 18:13:37 +0800

    webpack-simple有坑换用webpack

--- src/components/Main/Home/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div id="home">Home Page</div>
-</template>
-<script>
-export default {
-  
-}
-</script>
--- src/components/Main/Order/Order.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div id="order">Order Page</div>
-</template>
-<script>
-export default {
-  
-}
-</script>
