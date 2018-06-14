9207dc52f0d00e1554d32d10299c182f111865f9
243540158@qq.com <243540158@qq.com>
2018-06-14 15:17:02 +0800

    为cookEnd实现了Flex布局

--- src/components/CookEnd/CookEnd.vue
+++ src/components/CookEnd/CookEnd.vue
@@ -1,21 +1,23 @@
 <template>
   <div id='cookEndWrapper'>
-   <button type="button" @click="changeDisplayedList">{{buttonText}}</button>
+    <button type="button"  @click="changeDisplayedList">{{buttonText}}</button>
    <div  v-show="isAllListDisplayed">
       这是全部订单
    </div>
-   <div v-show="!isAllListDisplayed">
-    <div class="toDoOrderWrapperInCookEnd" v-for="order in allOrderToDo" :key="order.orderId">
-      <p>{{order.deskId}}号桌</p>
-      <p>下单时间 {{order.orderTime}}</p>
-      <p> 订单号 {{order.orderId}}</p>
-      <div class="wrapperOfDishesInToDoOrder" v-for="dish in order.dishList" :key="dish.dishId">
-        {{dish.dishName}} *  {{dish.num}} <button v-show="!dish.isDishCompleted">完成</button>
-      </div>
-      <p>备注: {{order.note}} </p>
-      <button v-show="!order.isOrderCompleted">全部完成</button>
+     <div v-show="!isAllListDisplayed">
+    <div  class="masonry">
+      <div  class="toDoOrderWrapperInCookEnd item" v-for="order in allOrderToDo" :key="order.orderId">
+        <div>{{order.deskId}}号桌</div>
+        <div>下单时间 {{order.orderTime}}</div>
+        <div> 订单号 {{order.orderId}}</div>
+        <div class="wrapperOfDishesInToDoOrder" v-for="dish in order.dishList" :key="dish.dishId">
+          {{dish.dishName}} *  {{dish.num}} <button v-show="!dish.isDishCompleted">完成</button>
+        </div>
+        <div>备注: {{order.note}} </div>
+        <button v-show="!order.isOrderCompleted">全部完成</button>
+        </div>
+       </div>
     </div>
-   </div>
   </div>
 </template>
 
@@ -36,7 +38,7 @@ export default {
       if (this.isAllListDisplayed) {
         this.buttonText = '全部订单'
       } else {
-        this.buttonText = '完成订单'
+        this.buttonText = '未做订单'
       }
       this.isAllListDisplayed = !this.isAllListDisplayed
     }
@@ -54,6 +56,7 @@ export default {
 </script>
 
 <style>
+
   .toDoOrderWrapperInCookEnd {
     border-color: black;
     border-width: 1px;
@@ -61,4 +64,26 @@ export default {
     padding-left: 5px;
     padding-right: 5px;
   }
+
+  html {
+  box-sizing: border-box;
+}
+
+.masonry {
+  display: flex;
+  flex-flow: row wrap;
+  margin-right: 5vw;
+  margin-left: 5vw; /* Adjustment for the gutter */
+  width: 90%;
+}
+
+ .item {
+  flex-shrink: 0;
+  flex-grow: 0;
+  flex-basis: auto;
+  height: auto;
+  min-width: 360px;
+  margin: 0 8px 8px 0; /* Some gutter */
+}
+
 </style>
