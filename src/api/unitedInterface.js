f88ceb94bb41503c79b20e78fe23ed63cfeff27f
243540158@qq.com <243540158@qq.com>
2018-06-20 11:34:39 +0800

    实现了用户的订单界面，能够查看自己的订单完成状况

--- src/api/unitedInterface.js
+++ src/api/unitedInterface.js
@@ -5,24 +5,7 @@
 import axios from 'axios'
 
 export default {
-  getDishesFromService (cb) {
-    console.log('getDishesFromService called')
-    axios.get('https://private-anon-1fd8f2407a-easymealorder.apiary-mock.com/dishes')
-      .then(cb)
-      .catch(error => {
-        console.log(error)
-      })
-  },
-
-  buyDishes (products, cb, errorCb) {
-    setTimeout(() => {
-      // simulate random checkout failure.
-      (Math.random() > 0.5 || navigator.userAgent.indexOf('PhantomJS') > -1)
-        ? cb()
-        : errorCb()
-    }, 100)
-  },
-
+  // 厨师用
   getAllToDoOrder (cb) {
     console.log('getAllToDoOrder called')
     axios.get('https://private-anon-1fd8f2407a-easymealorder.apiary-mock.com/menus')
@@ -32,6 +15,7 @@ export default {
       })
   },
 
+  // 厨师用
   finishADishInAOrder: function (orderId, dishId, cb, errorCb) {
     // todo: 向后端更新状态
     // 成功则cb
@@ -39,10 +23,37 @@ export default {
     // 失败则errorCb
   },
 
+  // 厨师用
   finishAOrder: function (orderIdk, cb, errorCb) {
     // todo：像后端更新状态
     // 成功则cb
     cb()
     // 失败则errorCb
+  },
+
+  // 消费者用
+  getDishesFromService (cb) {
+    console.log('getDishesFromService called')
+    axios.get('https://private-anon-1fd8f2407a-easymealorder.apiary-mock.com/dishes')
+      .then(cb)
+      .catch(error => {
+        console.log(error)
+      })
+  },
+
+  // 消费者用
+  buyDishes (products, cb, errorCb) {
+    setTimeout(() => {
+      // simulate random checkout failure.
+      (Math.random() > 0.5 || navigator.userAgent.indexOf('PhantomJS') > -1)
+        ? cb()
+        : errorCb()
+    }, 100)
+  },
+
+  // 消费者用
+  // 暂定用session作为标识
+  getAllMyOrder (userSession, cb) {
+    this.getAllToDoOrder(cb)
   }
 }
--- src/components/Main/Order/Order.vue
+++ src/components/Main/Order/Order.vue
@@ -1,6 +1,129 @@
 <template>
-    <div id="order">Order Page</div>
+  <div id='orderPage'>
+    <div  class="masonry">
+      <div  class="allOrderListInConsumerEnd item" v-for="order in allMyOrder" :key="order.orderId" d>
+        <div class="orderMetaData">{{order.deskId}}号桌    {{order.orderTime}}</div>
+        <div class="orderMetaData"> 订单号 {{order.orderId}}</div>
+        <hr/>
+
+        <div class="wrapperOfDishesInToDoOrder" v-for="dish in order.dishList" :key="dish.dishId">
+          <div id="dishWrapper">
+           <div id="dishNameItem">{{dish.dishName}}</div>
+           <div id="dishAmountItem">{{dish.num}}份</div>
+           <div id="dishCompletedItem">
+            <div id="ADishInAOrderIsFinishedText" v-show="dish.isDishCompleted">已完成</div>
+            <div id="ADishInAOrderIsNotFinishedText" v-show="!dish.isDishCompleted">未完成</div>
+           </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <div>备注: {{order.note}} </div>
+        </div>
+       </div>
+  </div>
 </template>
 <script>
-export default {}
+import service from '../../../api/unitedInterface'
+
+export default {
+  name: 'cookEndPage',
+  data: function () {
+    return {
+      allMyOrder: []
+    }
+  },
+  created () {
+    // 假装是用户订单
+    // todo：之后改回是客户用的接口
+    service.getAllMyOrder(
+      null,
+      (textGet) => { this.allMyOrder = textGet['data'] }
+    )
+  }
+}
 </script>
+
+<style>
+
+  .allOrderListInConsumerEnd {
+    border-color: black;
+    border-width: 1px;
+    border-style: solid;
+    padding-left: 5px;
+    padding-right: 5px;
+    padding-top: 5px;
+  }
+
+  html {
+  box-sizing: border-box;
+}
+
+.masonry {
+  display: flex;
+  flex-flow: row wrap;
+  margin-right: 5vw;
+  margin-left: 5vw; /* Adjustment for the gutter */
+  width: 90%;
+}
+
+.item {
+  flex-shrink: 0;
+  flex-grow: 0;
+  flex-basis: auto;
+  height: auto;
+  min-width: 360px;
+  margin: 0 8px 8px 0; /* Some gutter */
+}
+
+.orderMetaData {
+  font-size: 14px;
+  background-color: #e6cf8b;
+}
+
+#dishWrapper {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+#dishNameItem,#dishCompletedItem, #dishCompletedItem {
+  flex-shrink: 0;
+  flex-grow: 0;
+  flex-basis: auto;
+  height: auto;
+}
+
+#dishNameItem {
+  flex-shrink: 4;
+  flex-grow: 4;
+}
+
+#dishAmountItem {
+  flex-shrink: 1;
+  flex-grow: 1;
+}
+
+#dishCompletedItem {
+  flex-shrink: 1;
+  flex-grow: 1;
+}
+
+#allCompletedButton {
+  width: 100%;
+  margin-bottom: 5px;
+  background-color: #b56969;
+}
+
+#finishADishInAOrderButton {
+  background-color: #b56969;
+}
+
+#ADishInAOrderIsFinishedText {
+  color: #3cb035;
+}
+
+#ADishInAOrderIsNotFinishedText{
+  color: #b56969;
+}
+</style>
